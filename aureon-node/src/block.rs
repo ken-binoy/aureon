e87a209da59db095549154f79a03708032f28177
kenbinoy <binoy.ken@gmail.com>
2025-06-18 00:04:22 +0530

    5.1 block.rs dummy fix

--- aureon-node/src/block.rs
+++ aureon-node/src/block.rs
@@ -1,14 +1,12 @@
-
 use crate::types::{Block, Transaction};
 
 pub fn produce_block(transactions: Vec<Transaction>, pre_root: Vec<u8>, post_root: Vec<u8>) -> Block {
     Block {
         transactions,
+        previous_hash: "0000000000000000".to_string(), // dummy
+        nonce: 0,
+        hash: "deadbeef".to_string(), // dummy
         pre_state_root: pre_root,
         post_state_root: post_root,
-        previous_hash: todo!(),
-        nonce: todo!(),
-        hash: todo!(),
-        // Populate other fields if necessary
     }
 }
